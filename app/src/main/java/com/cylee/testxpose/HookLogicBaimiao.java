e9df5e584998d77626e21d2a1a8b1ea450c89f7b
cylee <cylee0909@qq.com>
2019-11-24 19:44:06 +0800

    添加baimiao hook

---
+++ app/src/main/java/com/cylee/testxpose/HookLogicBaimiao.java
@@ -0,0 +1,33 @@
+package com.cylee.testxpose;
+
+import android.util.Log;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by cylee on 2018/8/12.
+ */
+
+public class HookLogicBaimiao implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("package + "+lpparam.packageName+" launched");
+        if (lpparam.packageName.equals("com.uzero.baimiao")) {
+            XposedHelpers.findAndHookMethod("com.uzero.baimiao.domain.UserRecognizeInfo", lpparam.classLoader, "getRemainNormal", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                }
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Log.d("cylee", "raw result = "+param);
+                    param.setResult(1000);
+                }
+            });
+        }
+    }
+}
